e5a52c6478d472003e4c94a8efcadae31f678991
Marwa Mahmoud <marwa@bluskyint.com>
2024-09-12 11:06:41 +0300

    install client-side lib tailwind

---
+++ MVC Session02 Demo.Net8/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Session02_Demo.Net8.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
