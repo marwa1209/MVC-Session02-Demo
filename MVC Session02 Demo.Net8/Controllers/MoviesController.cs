8a493d682b6523d034d21c5f2adc443893f743ea
Marwa Mahmoud <marwa@bluskyint.com>
2024-09-12 10:57:40 +0300

    Action Parameters Binding

--- MVC Session02 Demo.Net8/Controllers/MoviesController.cs
+++ MVC Session02 Demo.Net8/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Session02_Demo.Net8.Models;
 
 namespace MVC_Session02_Demo.Net8.Controllers
 {
@@ -37,30 +38,41 @@ namespace MVC_Session02_Demo.Net8.Controllers
         //    RedirectResult result = new RedirectResult("https://www.facebook.com");
         //    return result;
         //}
-        [HttpGet]
-        [ActionName("test")]
-        public IActionResult Test(string id)
-        {
-            RedirectToActionResult result = new RedirectToActionResult("GetMovie", "Movies", new { id });
-            return result;
-        }
-        public IActionResult Test01(string id)
+        //[HttpGet]
+        //[ActionName("test")]
+        //public IActionResult Test(string id)
+        //{
+        //    RedirectToActionResult result = new RedirectToActionResult("GetMovie", "Movies", new { id });
+        //    return result;
+        //}
+        //public IActionResult Test01(string id)
 
-        {
-            //RedirectResult result = new RedirectResult("https://www.facebook.com");
-            //return result;
-            return Redirect("https://www.facebook.com");
-        }
-        public IActionResult GetMovie(string id)
-        {
-          //  ContentResult result = new ContentResult();
-           // result.Content = $"id={id}";
-            // result.ContentType = "text/html" ;
-            //  result.ContentType = "object/pdf" ;
-            return Content($"id={id}", "object/pdf");
-        }
+        //{
+        //    //RedirectResult result = new RedirectResult("https://www.facebook.com");
+        //    //return result;
+        //    return Redirect("https://www.facebook.com");
+        //}
+        //public IActionResult GetMovie(string id)
+        //{
+        //  //  ContentResult result = new ContentResult();
+        //   // result.Content = $"id={id}";
+        //    // result.ContentType = "text/html" ;
+        //    //  result.ContentType = "object/pdf" ;
+        //    return Content($"id={id}", "object/pdf");
+        //}
         //all action data types inherit from ActionResult
         //all of the implement iActionResult
+        #endregion
+        #region Action Parameters Binding
+        //1.Form
+        //2.Segment
+        //3.queryParams , string
+        //4.File
+
+        public IActionResult GetMovie(int Id,string Name ,Movies movie)
+        {
+            return Content($"id={Id} Name= {Name}");
+        }
         #endregion
     }
 }
---
+++ MVC Session02 Demo.Net8/Models/Movies.cs
@@ -0,0 +1,8 @@
+﻿namespace MVC_Session02_Demo.Net8.Models
+{
+    public class Movies
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
